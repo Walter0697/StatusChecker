847e57b01c8c0b64c968cf6721e97481b65a7002
Walter Cheng <walterpakwai@live.com>
2022-07-15 21:52:53 +0800

    Adding icons in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,16 @@
-import { defineNuxtConfig } from "nuxt";
+import { defineNuxtConfig } from "nuxt"
+import Icons from 'unplugin-icons/vite'
 
 export default defineNuxtConfig({
   css: ["~/assets/css/tailwind.css"],
+  vite: {
+    plugins: [
+      Icons({
+        // the feature below is experimental ⬇️
+        autoInstall: true
+      })
+    ]
+  },
   build: {
     postcss: {
       postcssOptions: {
