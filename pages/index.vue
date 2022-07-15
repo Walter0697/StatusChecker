8609db47cb8f1cbf91d443a542dd6ef1df5b7519
Walter Cheng <walterpakwai@live.com>
2022-07-15 12:24:36 +0800

    fixing issue for directly importing

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { LockClosedIcon } from '@heroicons/vue/solid'
+  import IconAccountBox from '~icons/mdi/account-box'
 </script>
 
 <template>
@@ -35,7 +35,7 @@
         <div>
           <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-              <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+              <icon-account-box style="font-size: 2em; color: white" />
             </span>
             Sign in
           </button>
